675ef2d0527d99144ce82bbbb5de4548b195c8f0
Admin <admin@MAC12.local>
2019-01-15 11:09:59 +0700

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Admin on 15/1/2562 BE.
+//  Copyright © 2562 AryMiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
